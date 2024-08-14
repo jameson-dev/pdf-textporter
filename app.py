e8d9fd8b99b580c97bccfb36d33199759e3b51a2
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-08-14 23:07:15 +0930

    Replace canvas with SimpleDocTemplate
    Canvas is a much lower-level system which is not required for this project. The use of SimpleDocTemplate is just easier

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os.path
 
 from pypdf import PdfReader, PdfWriter
 from reportlab.pdfgen import canvas
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase.ttfonts import TTFont
@@ -13,6 +15,7 @@ pdf_template_name = "template.pdf"
 pdf_output_name = "output.pdf"
 displayed_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor".upper()
 
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 PDF_PATH = os.path.join(ROOT_DIR, pdf_template_name)
 FONT_PATH = os.path.join(ROOT_DIR, font_dir_name)
@@ -22,12 +25,28 @@ pdfmetrics.registerFont(TTFont(text_font, FONT_PATH + f"{text_font}.ttf"))
 
 # Create temporary PDF
 packet = io.BytesIO()
-canvas = canvas.Canvas(filename=packet, pagesize=A4)
 
-# Set string parameters and save
-canvas.setFont(text_font, 15)
-canvas.drawString(10, 100, displayed_string)
-canvas.save()
+# canvas = canvas.Canvas(filename=packet, pagesize=A4)
+doc = SimpleDocTemplate(filename=packet, pagesize=A4
+                        )
+style = ParagraphStyle(
+    name="Default",
+    fontName=text_font,
+    fontSize=13,
+    borderColor="#000000",
+    borderWidth=1,
+    leading=15,
+    borderPadding=20
+)
+
+# Set Paragraph parameters and save
+para_pager = Paragraph(displayed_string, style=style)
+
+# Create a vertical spacer
+top_spacer = Spacer(0, 50)
+
+# Build the document with specified flowables
+doc.build([top_spacer, para_pager])
 
 # Create new PDF
 packet.seek(0)
