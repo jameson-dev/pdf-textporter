cd65a512269b270f753149336e78d376e2f6d590
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-08-16 23:47:09 +0930

    Refactor code for use of functions

--- app.py
+++ app.py
@@ -19,48 +19,72 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 PDF_PATH = os.path.join(ROOT_DIR, pdf_template_name)
 FONT_PATH = os.path.join(ROOT_DIR, font_dir_name)
 
-# Register custom font (Consolas) to ReportLib
-pdfmetrics.registerFont(TTFont(text_font, FONT_PATH + f"{text_font}.ttf"))
 
-# Create temporary PDF
-packet = io.BytesIO()
+def register_font(font, path) -> None:
+    pdfmetrics.registerFont(TTFont(text_font, FONT_PATH + f"{text_font}.ttf"))
 
-doc = SimpleDocTemplate(filename=packet, pagesize=A4)
 
-# Instantiate styling class
-style = ParagraphStyle(
-    name="Default",
-    fontName=text_font,
-    fontSize=13,
-    borderColor="#000000",
-    borderWidth=1,
-    leading=15,
-    borderPadding=20
-)
+def create_temp_pdf(string, font) -> io.BytesIO:
+    # Create temporary PDF
+    packet = io.BytesIO()
 
-# Paragraph parameters
-para_pager = Paragraph(displayed_string, style=style)
+    doc = SimpleDocTemplate(filename=packet, pagesize=A4)
 
-# Create a vertical spacer
-top_spacer = Spacer(0, 50)
+    # Instantiate styling class
+    style = ParagraphStyle(
+        name="Default",
+        fontName=font,
+        fontSize=13,
+        borderColor="#000000",
+        borderWidth=1,
+        leading=15,
+        borderPadding=20
+    )
 
-# Build the document with specified flowables
-doc.build([top_spacer, para_pager])
+    # Paragraph parameters
+    para_pager = Paragraph(string, style=style)
 
-# Build the new PDF we'll be using
-packet.seek(0)
-new_pdf = PdfReader(packet)
+    # Create a vertical spacer
+    top_spacer = Spacer(0, 50)
 
-# Read existing template PDF
-existing_pdf = PdfReader(open(PDF_PATH, "rb"))
-output = PdfWriter()
+    # Build the document with specified flowables
+    doc.build([top_spacer, para_pager])
 
-# Overlay new PDF onto template PDF
-page = existing_pdf.get_page(0)
-page.merge_page(new_pdf.get_page(0))
-output.add_page(page)
+    # Build the new PDF we'll be using
+    packet.seek(0)
 
-# Save output to a file
-outputStream = open(pdf_output_name, "wb")
-output.write(outputStream)
-outputStream.close()
+    return packet
+
+
+def overlay_pdfs(overlay_pdf, template_pdf, pdf_name ):
+    new_pdf = PdfReader(overlay_pdf)
+
+    # Read existing template PDF
+    existing_pdf = PdfReader(open(template_pdf, "rb"))
+    output = PdfWriter()
+
+    # Overlay new PDF onto template PDF
+    page = existing_pdf.get_page(0)
+    page.merge_page(new_pdf.get_page(0))
+    output.add_page(page)
+
+    # Save output to a file
+    output_stream = open(pdf_name, "wb")
+    output.write(output_stream)
+    output_stream.close()
+
+
+def main():
+
+    # Register custom font
+    register_font(text_font, FONT_PATH)
+
+    # Create temporary PDF with desired string
+    temp_pdf = create_temp_pdf(displayed_string, text_font)
+
+    # Overlay PDFs
+    overlay_pdfs(temp_pdf, PDF_PATH, pdf_output_name)
+
+
+if __name__ == "__main__":
+    main()
