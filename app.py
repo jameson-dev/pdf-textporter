9fde9d626ed22368784204f7671badd7131706bf
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-08-25 21:57:32 +0930

    Improve error handling for overlay function

--- app.py
+++ app.py
@@ -60,21 +60,24 @@ def create_temp_pdf(string, font) -> io.BytesIO:
 
 
 def overlay_pdfs(overlay_pdf, template_pdf, pdf_name):
-    new_pdf = PdfReader(overlay_pdf)
+    try:
+        new_pdf = PdfReader(overlay_pdf)
 
-    # Read existing template PDF
-    with open(template_pdf, "rb") as f:
-        existing_pdf = PdfReader(f)
-        output = PdfWriter()
+        # Read existing template PDF
+        with open(template_pdf, "rb") as f:
+            existing_pdf = PdfReader(f)
+            output = PdfWriter()
 
-    # Overlay new PDF onto template PDF
-    page = existing_pdf.get_page(0)
-    page.merge_page(new_pdf.get_page(0))
-    output.add_page(page)
+        # Overlay new PDF onto template PDF
+        page = existing_pdf.get_page(0)
+        page.merge_page(new_pdf.get_page(0))
+        output.add_page(page)
 
-    # Save output to a file
-    with open(pdf_name, "wb") as output_stream:
-        output.write(output_stream)
+        # Save output to a file
+        with open(pdf_name, "wb") as output_stream:
+            output.write(output_stream)
+    except Exception as e:
+        RuntimeError(f"Unable to overlay PDF: {e}")
 
 
 def main():
