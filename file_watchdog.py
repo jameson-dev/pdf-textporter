69f112e5deecc9bf4c5847d7ce7241bb99df1a33
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-08-27 15:53:22 +0930

    Fix watchdog trying to read config too early

--- file_watchdog.py
+++ file_watchdog.py
@@ -5,22 +5,21 @@ from watchdog.events import FileSystemEventHandler
 from loguru import logger
 from config import read_config
 
-config_values = read_config()
-
 
 class Watcher:
-    WATCHDOG_DIR = config_values['msgs_path']
-
     def __init__(self):
         self.observer = Observer()
 
     def run(self):
-        if not os.path.isdir(self.WATCHDOG_DIR):
-            logger.warning(f"{self.WATCHDOG_DIR} directory does not exist. Creating it now...")
-            os.mkdir(self.WATCHDOG_DIR)
+        config_values = read_config()
+        watchdog_dir = config_values['msgs_path']
+        logger.info(f"Watchdog is now monitoring '{watchdog_dir}'")
+        if not os.path.isdir(watchdog_dir):
+            logger.warning(f"{watchdog_dir} directory does not exist. Creating it now...")
+            os.mkdir(watchdog_dir)
         try:
             event_handler = Handler()
-            self.observer.schedule(event_handler, self.WATCHDOG_DIR, recursive=False)
+            self.observer.schedule(event_handler, watchdog_dir, recursive=False)
             self.observer.start()
         except FileNotFoundError as e:
             logger.error(f"An error occurred: {e}")
