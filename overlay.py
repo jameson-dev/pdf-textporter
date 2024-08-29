19ca1fdacfb4bae2646c18e65f0fec743e19a85a
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-08-29 23:50:24 +0930

    Fix file being closed before finishing with it

--- overlay.py
+++ overlay.py
@@ -11,10 +11,10 @@ def overlay_pdfs(overlay_pdf, template_pdf, pdf_name):
             existing_pdf = PdfReader(f)
             output = PdfWriter()
 
-        # Overlay new PDF onto template PDF
-        page = existing_pdf.get_page(0)
-        page.merge_page(new_pdf.get_page(0))
-        output.add_page(page)
+            # Overlay new PDF onto template PDF
+            page = existing_pdf.get_page(0)
+            page.merge_page(new_pdf.get_page(0))
+            output.add_page(page)
 
         # Save output to a file
         with open(pdf_name, "wb") as output_stream:
