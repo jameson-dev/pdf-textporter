0773e07663623a6f984a52195d8fe677cef21379
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-08-29 23:48:41 +0930

    Add logging to register_font

--- register_font.py
+++ register_font.py
@@ -7,7 +7,9 @@ from loguru import logger
 def register_font(font_name: str, font_path: str) -> None:
     full_path = os.path.join(font_path, f"{font_name}.ttf")
     try:
+        logger.debug(f"Registering font at {full_path}")
         pdfmetrics.registerFont(TTFont(font_name, full_path))
+        logger.info(f"Font '{font_name}' has been registered")
     except Exception as e:
         logger.warning(f"Unable to register font: {e}")
         logger.info("Falling back to default font...")
