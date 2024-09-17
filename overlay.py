741a38409a007fc79fc6b57a952f15a1b636ed74
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-09-17 11:01:47 +0930

    Reimplement frames and sufficient  debugging

--- overlay.py
+++ overlay.py
@@ -5,11 +5,27 @@ from print_pdf import print_pdf
 
 def overlay_pdfs(overlay_pdf, template_pdf, pdf_name):
     try:
+        # Debugging: Print the number of pages in overlay PDF
+        if overlay_pdf.getvalue():
+            logger.debug(f"Overlay PDF stream contains data: {overlay_pdf.getvalue()}")
+        else:
+            logger.error("Overlay PDF stream is empty. There is an issue.")
+
         new_pdf = PdfReader(overlay_pdf)
+
+        # Debugging: Print the number of pages in the overlay PDF
+        logger.debug(f"Number of pages in overlay PDF: {len(new_pdf.pages)}")
 
         # Read existing template PDF
         with open(template_pdf, "rb") as f:
             existing_pdf = PdfReader(f)
+
+            # Ensure both PDFs have at least one page
+            if len(new_pdf.pages) < 1:
+                raise ValueError("Overlay PDF does not have any pages.")
+            if len(existing_pdf.pages) < 1:
+                raise ValueError("Template PDF does not have any pages.")
+
             output = PdfWriter()
 
             # Overlay new PDF onto template PDF
