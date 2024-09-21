6d29928b36407bbb922cc849f2450b4e0f54dd25
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-09-21 21:11:19 +0930

    Fix file path naming

--- tasking_sheets/module_check.py
+++ tasking_sheets/module_check.py
@@ -1,7 +1,11 @@
 import importlib.metadata
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+requirements_file = os.path.join(parent_dir, 'requirements.txt')
 
 
-def load_requirements(filename='../requirements.txt'):
+def load_requirements(filename=requirements_file):
     with open(filename, 'r') as file:
         requirements = [line.strip() for line in file if line.strip() and not line.startswith('#')]
     return requirements
